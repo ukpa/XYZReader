45dbd199fc22a5fffa00e86564012c52721042d4
Unnikrishnan Patel <patel.unnikrishnan@gmail.com>
2016-04-29 00:41:31 +0530

    fab elevation added

--- src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -17,7 +17,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ShareCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.graphics.Palette;
-import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.text.format.DateUtils;
 import android.text.method.LinkMovementMethod;
